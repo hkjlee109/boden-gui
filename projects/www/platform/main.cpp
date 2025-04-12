9d646df1dde6d4f45e41def8ab02a083fcab334f
Kyung <hkjlee109@gmail.com>
2025-04-12 19:42:25 +0200

    Import app projects.

--- projects/www/platform/main.cpp
+++ projects/www/platform/main.cpp
@@ -1,4 +1,5 @@
 #include <boden/context.hpp>
+#include <app/main_view_controller.hpp>
 
 #include <emscripten.h>
 #include <emscripten/bind.h>
@@ -8,4 +9,6 @@ EMSCRIPTEN_BINDINGS(main)
 {
     emscripten::value_object<boden::context_t>("context_t");
 
+    emscripten::class_<app::main_view_controller_t>("main_view_controller")
+        .constructor<>();
 }
