f436192b55bb0d4f6fb4bab48d9320603bfd7669
Kyung <hkjlee109@gmail.com>
2025-04-15 15:02:20 +0200

    www: modularize.

--- projects/www/main.js
+++ projects/www/main.js
@@ -1,15 +1,49 @@
 import { setup as webgl_setup } from "./webgl/index.js"
 
-main();
+import createModule from './build/boden-www.js';
 
-function main() {
+createModule().then((Module) => {
+    Module.webgl_setup = webgl_setup;
+
+    main(Module);
+});
+
+let renderer;
+let main_view_controller;
+
+function main(module) {
+    console.log('# Starting... ');
+
     const canvas = document.querySelector("#gl-canvas");
     const gl = canvas.getContext("webgl");
 
-    if(gl === null) {
+    if (gl === null) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
 
     webgl_setup(gl);
+    
+    renderer = new module.webgl_renderer_t();
+
+    let rect = new module.rect_t(0, 0, 640, 480);
+    main_view_controller = new module.main_view_controller_t(rect);
+    rect.delete();
+
+    let context = new module.context_t();
+    context.renderer = renderer;
+
+    main_view_controller.draw(context);
+
+    context.delete();
+
+    if (renderer) {
+        renderer.delete();
+        renderer = null;
+    }
+
+    if (main_view_controller) {
+        main_view_controller.delete();
+        main_view_controller = null;
+    }
 }
--- projects/www/platform/library.js
+++ projects/www/platform/library.js
@@ -21,7 +21,7 @@ addToLibrary({
     
         gl.clearColor(0.0, 0.0, 1.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-
+        
         /*
          * struct command_t
          * uint32_t count;
