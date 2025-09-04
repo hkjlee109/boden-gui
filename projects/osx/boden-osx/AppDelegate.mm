da004805d4a66c1fee15c3a0dd143ea602802203
Kyung <hkjlee109@gmail.com>
2025-09-04 09:57:22 +0200

    Cleanup.

--- projects/osx/boden-osx/AppDelegate.mm
+++ projects/osx/boden-osx/AppDelegate.mm
@@ -1,22 +1,25 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+#import "platform/osx_backend.hpp"
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate {
-    NSWindow *window;
+    NSWindow *_window;
+    platform::osx_backend_t _backend;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480)
+    _window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480)
                                          styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskResizable
                                            backing:NSBackingStoreBuffered
                                              defer:YES];
-    window.contentViewController = [ViewController new];
-    [window center];
-    [window orderFront:self];
+    _window.contentViewController = [ViewController new];
+    [_window center];
+    [_window orderFront:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
